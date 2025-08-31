29035484fb8b63d37d93a9b69538398f48edf762
mohamednura200 <mohamednura200@gmial.com>
2025-08-31 20:00:52 +0300

    save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./checkout/oderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
